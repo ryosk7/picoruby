959e1875902566b509b7907e203994f2c71954b5
ryosk7 <uchiryo7@gmail.com>
2025-01-26 02:38:57 +0900

    Added mqtt gems for build

--- build_config/r2p2_wifi-cortex-m0plus.rb
+++ build_config/r2p2_wifi-cortex-m0plus.rb
@@ -48,6 +48,7 @@ MRuby::CrossBuild.new("r2p2_wifi-cortex-m0plus") do |conf|
   conf.gembox "stdlib"
   conf.gembox "utils"
   conf.gem core: 'picoruby-net'
+  conf.gem core: 'picoruby-mqtt'
   # For some reason, picoruby-net doesn't have dependency on picoruby-cyw43
   conf.gem core: 'picoruby-cyw43'
 
